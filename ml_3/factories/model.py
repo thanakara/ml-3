22ce6f5dfd9e2f228bdbbe4f72ca64bba644cbcc
thanakara <dngus1717@gmail.com>
2025-07-06 00:22:00 +0300

    yaml_files

--- ml_3/factories/model.py
+++ ml_3/factories/model.py
@@ -1,26 +1,38 @@
 import os
+os.environ["TF_CPP_MIN_LOG_LEVEL"] = "2"
 import tensorflow as tf
 from functools import partial
 
 
-def model_factory() -> tf.keras.Model:
-    Dense = partial(tf.keras.layers.Dense, activation="relu")
-    pass
 
-class ModelFactory(object):
-    def __init__(self, seed=42) -> None:
-        self.seed = seed
-    
-    def _build(self, num_hidden: int, name: str) -> tf.keras.Sequential:
-        Dense = partial(tf.keras.layers.Dense, activation="relu")
-        tf.keras.utils.set_random_seed(self.seed)
-        model = tf.keras.Sequential([
-            tf.keras.Input([28, 28]),
-            tf.keras.layers.Flatten()
-        ], name=name)
-        units = 2 ** (num_hidden + 4)
-        for _ in range(num_hidden):
-            model.add(Dense(units=units))
-            units //= 2
-        model.add(tf.keras.layers.Dense(10, activation="softmax"))
-        return model
+def get_fc_part(layers) -> tf.keras.Model:
+    input_dim, *hidden, output_layer = layers
+    Dense = partial(tf.keras.layers.Dense)
+    model = tf.keras.Sequential([
+        tf.keras.Input(shape=input_dim),
+        tf.keras.layers.Flatten()
+    ])
+    for layer in hidden:
+        units, activation = layer
+        model.add(Dense(units=units, activation=activation))
+    output_dim, activation = output_layer
+    model.add(Dense(units=output_dim, activation=activation))
+    return model
+
+def get_cnn_part(layers):
+    (x, y), *hidden = layers
+    Conv = partial(tf.keras.layers.Conv2D)
+    model = tf.keras.Sequential([
+        tf.keras.Input([x, y]),
+        tf.keras.layers.Reshape([x, y, 1])
+    ])
+    for layer in hidden:
+        filters, kernel, strides, padding, activation = layer
+        model.add(Conv(filters=filters, kernel_size=kernel, strides=strides,
+                       padding=padding, activation=activation))
+        model.add(tf.keras.layers.MaxPool2D())
+    return model
+
+
+def model_factory() -> tf.keras.Model:
+    pass
