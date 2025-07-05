3e4da5d8dd9bf771f015b5274705e14b3f130237
thanakara <dngus1717@gmail.com>
2025-07-05 22:03:39 +0300

    dataset_factory fixes

--- ml_3/factories/dataset.py
+++ ml_3/factories/dataset.py
@@ -1,8 +1,8 @@
 from dataclasses import dataclass
 import tensorflow as tf
-import numpy as np
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import MinMaxScaler
+
 
 @dataclass
 class Dataset:
@@ -11,7 +11,7 @@ class Dataset:
     test: tf.data.Dataset
     
 def get_gs_dataset(
-    ds: tf.keras.Dataset,
+    ds,                 # TODO: hint
     test_size: int,
     seed: int,
     scale: bool=True,
@@ -27,8 +27,11 @@ def get_gs_dataset(
         )
     
     if scale:
+        shape = X_train.shape[1:]
         scaler = MinMaxScaler(feature_range=(0, 1))
-        X_train, X_valid, X_test = [scaler(i) for i in (X_train, X_valid, X_test)]
+        X_train, X_valid, X_test = [set_.ravel().reshape(-1, 1) for set_ in (X_train, X_valid, X_test)]
+        X_train, X_valid, X_test = [scaler.fit_transform(set_) for set_ in (X_train, X_valid, X_test)]
+        X_train, X_valid, X_test = [set_.reshape(-1, *shape) for set_ in (X_train, X_valid, X_test)]
     
     
     return Dataset(
@@ -41,14 +44,14 @@ def get_gs_dataset(
 def dataset_factory(name: str, test_size: int | float, seed: int, scale: bool=True) -> Dataset:
     if name == "mnist":
         return get_gs_dataset(
-            ds=tf.keras.dataset.mnist,
+            ds=tf.keras.datasets.mnist,
             test_size=test_size,
             seed=seed,
             scale=scale
             )
     elif name == "fashion_mnist":
         return get_gs_dataset(
-            ds=tf.keras.dataset.fashion_mnist,
+            ds=tf.keras.datasets.fashion_mnist,
             test_size=test_size,
             seed=seed,
             scale=scale
