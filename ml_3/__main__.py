304e67b0bcb2feca014189beaffb1df61bdc21c1
thanakara <dngus1717@gmail.com>
2025-07-05 14:13:19 +0300

    settings.yaml; since defaults on config.yaml has - settings & - _self_

--- ml_3/__main__.py
+++ ml_3/__main__.py
@@ -2,7 +2,6 @@ import hydra
 import logging
 from ml_3.definitions.constans import CONFIG_DIR
 from conf.config import MyConfig
-from omegaconf import OmegaConf
 
 
 logger = logging.getLogger(__name__)
@@ -11,9 +10,8 @@ config_path = str(CONFIG_DIR)
 @hydra.main(version_base=None, config_path=config_path, config_name="config")
 def main(cfg: MyConfig):
     logger.info("INFO level msg.")
-    logger.info(hydra.utils.get_original_cwd())
-    logger.debug(OmegaConf.to_yaml(cfg))
-    logger.debug(hydra.core.hydra_config.HydraConfig.get().runtime.output_dir)
+    return cfg
+
 
 if __name__ == "__main__":
     main()
--- ml_3/utils/callbacks.py
+++ ml_3/utils/callbacks.py
@@ -1,10 +1,35 @@
-import hydra
-from omegaconf import DictConfig
+import logging
+from typing import Any
+from conf.config import MyConfig
+from hydra.types import TaskFunction
+from hydra.core.utils import JobReturn
 from hydra.experimental.callback import Callback
 
+import pathlib
+import pandas as pd
+
 class MyCallback(Callback):
-    def __init__(self, blob: str) -> None:
-        self.blob = blob
+
+    def __init__(self) -> None:
+        self.log = logging.getLogger(f"{__name__}.{self.__class__.__name__}")
     
-    def on_job_end(self, config: DictConfig, **kwargs) -> None:
-        print("Job ended...")
+    def on_job_start(self,
+                     config: MyConfig,
+                     *,
+                     task_function: TaskFunction,
+                     **kwargs: Any) -> None:
+        print("Job started...\n")
+
+    def on_job_end(self,
+                   config: MyConfig,
+                   job_return: JobReturn,
+                   **kwargs) -> None:
+        
+        output_dir = pathlib.Path(config.hydra.runtime.output_dir)
+        output_dir.mkdir(parents=True, exist_ok=True)
+
+        configuration = job_return.return_value
+        configuration = pd.DataFrame(configuration)
+        configuration.T.to_json(output_dir.joinpath("conf.json"))
+
+        print("\nJob ended.")
