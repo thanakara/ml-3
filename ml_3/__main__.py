a5fc79ea44715077d1d5e672148dc0eac3effa5e
thanakara <dngus1717@gmail.com>
2025-07-19 17:05:17 +0300

    add Builders

--- ml_3/__main__.py
+++ ml_3/__main__.py
@@ -7,33 +7,21 @@ import logging
 from ml_3.definitions.constants import CONFIG_PATH
 from omegaconf import OmegaConf, DictConfig
 from ml_3.model.layers import flatten, calc_channels
+from ml_3.factories.model import KerasBuilder
+
 OmegaConf.register_new_resolver(name="flatten", resolver=flatten)
 OmegaConf.register_new_resolver(name="calc_channels", resolver=calc_channels)
-
-from ml_3.factories.dataset import dataset_factory
-from ml_3.factories.model import keras_builder
-from ml_3.model.train import compile_and_fit
-
 
 logger = logging.getLogger(__name__)
 config_path = str(CONFIG_PATH)
 
 @hydra.main(version_base=None, config_path=config_path, config_name="config")
 def main(config: DictConfig):
-    Dataset = dataset_factory(
-        name=config.dataset.name,
-        test_size=config.test_size,
-        seed=config.seed
-    )
-    model = keras_builder(layers=config.model.layers)
-    history = compile_and_fit(
-        model=model,
-        train_ds=Dataset.train,
-        valid_ds=Dataset.valid,
-        backend=config.backend
-    )
+    OmegaConf.resolve(config)
 
-    return history
+    builder = hydra.utils.call(config.backend.builder)
+    model = builder.build(config.model.layers)
+    print(model.__class__)
 
 if __name__ == "__main__":
     main()
--- ml_3/factories/model.py
+++ ml_3/factories/model.py
@@ -1,27 +1,72 @@
-import tensorflow as tf
-from hydra.utils import instantiate
-from ml_3.model.layers import Dense, Conv2D
-from dataclasses import asdict
+from abc import ABC, abstractmethod
+
+import torch
+import torch.nn as nn
+from hydra.utils import call, get_class
+from tensorflow import keras
+
+from ml_3.model.layers import Dense
 
 
-def keras_builder(layers: list) -> tf.keras.Model:
-    model = tf.keras.Sequential()
+class Builder(ABC):
+    @abstractmethod
+    def build(self, layers):
+        pass
 
-    for layer, args in layers.items():
-        if "flatten" in layer:
-            model.add(tf.keras.layers.Flatten(input_shape=args.input_shape))
-        else:
-            layer_args = instantiate(args)
-            if isinstance(layer_args, Dense):
-                layer_args = asdict(layer_args)
-                layer_args.pop("in_units")
-                out_units = layer_args.pop("out_units")
-                model.add(tf.keras.layers.Dense(
-                    units=out_units,
-                    **layer_args
-                ))
-            elif isinstance(layer_args, Conv2D):
-                layer_args = asdict(layer_args)
-                model.add(tf.keras.layers.Conv2D(**layer_args))
 
-    return model
+class TorchBuilder(Builder):
+    def __init__(self):
+        pass
+
+    def build(self, layers) -> nn.Module:
+        model = torch.nn.Sequential()
+
+        for name, layer in layers.items():
+            layer_cls = get_class(layer.cls)
+            args = call(layer.args) if layer.args else {}
+            module = layer_cls(**self.torch_args(args=args) if args else args)
+            model.add_module(name=name, module=module)
+
+        return model
+
+    def torch_args(self, args: Dense) -> dict:
+        return {
+            "in_features": args.in_units,
+            "out_features": args.out_units,
+            "bias": args.use_bias,
+            "activation": args.activation,
+            "kernel_initializer": args.kernel_initializer,
+            "bias_initializer": args.bias_initializer,
+            "kernel_regularizer": args.kernel_regularizer,
+            "bias_regularizer": args.bias_regularizer,
+            "activity_regularizer": args.activity_regularizer,
+            "kernel_constraint": args.kernel_constraint,
+            "bias_constraint": args.bias_constraint,
+        }
+
+
+class KerasBuilder(Builder):
+    def build(self, layers):
+        model = keras.Sequential()
+
+        for _, layer in layers.items():
+            layer_cls = get_class(layer.cls)
+            args = call(layer.args) if layer.args else {}
+            module = layer_cls(**self.keras_args(args=args) if args else args)
+            model.add(module)
+
+        return model
+
+    def keras_args(self, args: Dense) -> dict:
+        return {
+            "units": args.out_units,
+            "use_bias": args.use_bias,
+            "activation": args.activation,
+            "kernel_initializer": args.kernel_initializer,
+            "bias_initializer": args.bias_initializer,
+            "kernel_regularizer": args.kernel_regularizer,
+            "bias_regularizer": args.bias_regularizer,
+            "activity_regularizer": args.activity_regularizer,
+            "kernel_constraint": args.kernel_constraint,
+            "bias_constraint": args.bias_constraint,
+        }
